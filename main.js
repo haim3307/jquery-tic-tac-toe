83c43b28ac69f862c898a3a84d36d08904b86610
haim3307 <haim3307@gmail.com>
2020-03-27 15:50:32 +0300

    X-O Commit starting from 2017 v3
    Signed-off-by: haim3307 <haim3307@gmail.com>

--- main.js
+++ main.js
@@ -6,34 +6,10 @@
 /*global $, window, document */
 
 // Simple jQuery event handler
-var X1 = 'X',
-    O1 = 'O',
-    Y1 = O1,
-    matchFlagIs = false,
-    gameGrid;
 
 $(document).ready(function () {
-    $('.gridOption').click(function () {
-        gameGrid = $(this).val();;
-        createBoard(gameGrid);
-        $('td').click(function () {
-            console.log('phase 1.5');
-
-            var thisTD = $(this);
-            XOGame(gameGrid, thisTD);
-
-        });
-    });
-
-
-
-
-
-});
-
-function createBoard(gameNet) {
-    console.log('phase 1');
-    var XOGameTab = '';
+    var XOGameTab = '',
+        gameNet = 9;
     for (var i = 0; i < gameNet; i++) {
         XOGameTab += '<tr>';
         for (var x = 0; x < gameNet; x++)
@@ -41,60 +17,56 @@ function createBoard(gameNet) {
         XOGameTab += '</tr>';
     }
     $('table').html(XOGameTab);
-}
-
-function XOGame(gameNet, thisTD) {
-    console.log('phase 2');
-    console.log(gameNet);
-    var cTD = thisTD;
-    if (cTD.hasClass('clicked') || matchFlagIs) return;
-    var tdIndex = cTD.index() + 1,
-        trIndex = cTD.parent().index() + 1;
-
-    if (Y1 == O1) {
-        Y1 = X1;
-        cTD.addClass('clicked clickedX');
-    } else {
+    var X1 = 'X',
+        O1 = 'O',
         Y1 = O1;
-        cTD.addClass('clicked clickedO');
+    $('td').click(function () {
 
-    }
-
-    cTD.text(Y1);
-    var Yis = $(this).text();
+        var cTD = $(this);
+        if (cTD.hasClass('clicked')) return;
 
 
-    function checkIt(i, roof, two, one) {
-        var prvfinal, final = [];
-        var g = gameNet;
-        while (i < roof) {
-            console.log(i);
-            var mycommand = $('tr:nth-child(' + eval(two) + ') td:nth-child(' + eval(one) + ')').text()
+        var tdIndex = cTD.index() + 1,
+            trIndex = cTD.parent().index() + 1;
 
-            var innerT = mycommand;
-            final[i - 1] = innerT;
-            i++;
-            g -= 1;
-            console.log(final);
+        if (Y1 == O1) {
+            Y1 = 'X';
+            cTD.addClass('clicked clickedX');
+        } else {
+            Y1 = 'O';
+            cTD.addClass('clicked clickedO');
+
         }
-        var finalIs = final.reduce(function (a, b) {
-            return (a == b) ? a : NaN;
-        });
-        if (finalIs) {
-            WinTitle(finalIs[0] + ' won the game!');
-            matchFlagIs = true;
+        $(this).text(Y1);
+        var Yis = $(this).text();
+        function checkIt(i, roof, choose) {
+            var final = [];
+            while (i < roof) {
+                var mycommand = $('tr:nth-child(' + two + ') td:nth-child(' + i + ')').text()
+                var innerT = mycommand;
+                final[i - 1] = innerT;
+                i++;
+                g -= 1;
+            }
+            var finalIs=final.reduce(function (a, b) {
+                return (a==b) ? a : NaN;
+            });
+            if (finalIs) {
+                alert('win');
+
+            }
+
+
         }
+        checkIt(1, gameNet + 1, trIndex);
+        checkIt(1, gameNet + 1, tdIndex);
+        //checkIt('1', gameNet + 1, 'i', 'i');
+        //checkIt('1', gameNet + 1, 'g', 'i');
+
+    })
 
 
-    }
-    checkIt('1', gameNet, trIndex, 'i');
-    checkIt('1', gameNet, 'i', tdIndex);
-    checkIt('1', gameNet, 'i', 'i');
-    checkIt('1', gameNet, 'g', 'i');
-
-    function WinTitle(textIs) {
-        $('h1').text(textIs);
-    }
 
 
-}
+
+});
